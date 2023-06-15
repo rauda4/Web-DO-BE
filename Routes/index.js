e95d93e4862444176feb50d2b207230960a03c67
rafikaauda <rafika.auda4@gmail.com>
2023-06-15 21:04:57 +0700

    add login handler with jwt

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const UserRouter = require("./User");
 const DiamondRouter = require("./Diamond");
+const login = require("../controllers/Login.controller");
 
 router.use("/users", UserRouter)
 router.use("/diamond", DiamondRouter)
+
+// login handler
+router.post("/auth/login", login)
 
 module.exports = router
