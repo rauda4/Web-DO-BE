c3e25bd2feeccb69ee557d0c276bc10db7aa6fd5
rafikaauda <rafika.auda4@gmail.com>
2023-07-31 21:44:11 +0700

    add product router

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const UserRouter = require("./User");
 const DiamondRouter = require("./Diamond");
+const ProductRouter = require("./Products")
 const login = require("../controllers/Login.controller");
 
 router.use("/users", UserRouter)
 router.use("/diamond", DiamondRouter)
+router.use("/product", ProductRouter)
 
 // login handler
 router.post("/auth/login", login)
