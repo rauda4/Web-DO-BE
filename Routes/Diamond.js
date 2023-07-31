6fbe6dc027623b9ae79fb2ef9b0d8313123eed41
rafikaauda <rafika.auda4@gmail.com>
2023-07-31 21:45:42 +0700

    update schema, etc

--- Routes/Diamond.js
+++ Routes/Diamond.js
@@ -1,8 +1,10 @@
 const diamondController = require("../controllers/Diamond.controller")
+const productController = require("../controllers/Product.controller")
 const Router = require("express").Router()
 
 Router.post("/", diamondController.createDataDiamond)
 Router.get("/", diamondController.getDiamond)
+Router.get("/product", productController.getProduct)
 Router.get("/:id", diamondController.getDiamondById)
 Router.put("/:id", diamondController.updateData)
 Router.delete("/:id", diamondController.deleteDiamond)
