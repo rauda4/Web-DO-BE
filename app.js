6b1d750e95e3c99a7443767d94ff4e0a8139259e
rafikaauda <rafika.auda4@gmail.com>
2023-07-31 21:40:37 +0700

    update folder public

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const flash = require("express-flash");
 const cookieParser = require("cookie-parser");
 const routes = require("./Routes")
+const cors = require('cors');
 require("dotenv").config();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static("./views"));
+app.use(express.static("Public"));
+
+app.use(cors())
 app.use(cookieParser());
 app.use(flash());
 
