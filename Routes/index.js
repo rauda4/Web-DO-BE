3dc165c1684669fb58166dd0c25e7f4e41ee759c
rafikaauda <rafika.auda4@gmail.com>
2023-08-01 19:38:30 +0700

    improve endpoint auth and user

--- Routes/index.js
+++ Routes/index.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const UserRouter = require("./User");
 const DiamondRouter = require("./Diamond");
 const ProductRouter = require("./Products")
-const login = require("../controllers/Login.controller");
+const AuthRouter = require("./Auth")
 
 router.use("/users", UserRouter)
 router.use("/diamond", DiamondRouter)
 router.use("/product", ProductRouter)
 
-// login handler
-router.post("/auth/login", login)
+// Auth handler
+router.use("/auth", AuthRouter)
 
 module.exports = router
