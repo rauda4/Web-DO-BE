ce4a701f381a3676e0a85eed127ca5f10a7348cc
rafikaauda <rafika.auda4@gmail.com>
2023-08-01 19:38:17 +0700

    improve endpoint auth and user

--- Routes/User.js
+++ Routes/User.js
@@ -2,11 +2,10 @@ const UserController = require("../controllers/User.controller");
 const authOnly = require("../middlewares/auth");
 const Router = require("express").Router();
 
-Router.post("/", authOnly, UserController.CreateUser)
-Router.get("/", authOnly, UserController.getUser)
-Router.get("/:id", authOnly, UserController.GetUserById)
+Router.get("/", UserController.getUser)
+Router.get("/:id", UserController.GetUserById)
 Router.get("/search/:key", authOnly, UserController.GetUserByQuery)
 Router.put("/:id", authOnly, UserController.updateUser)
-Router.delete("/:id", authOnly, UserController.deleteUser)
+Router.delete("/:id", UserController.deleteUser)
 
 module.exports = Router
