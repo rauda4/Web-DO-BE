76a47f315f8fc3ff839441d43cf6cac5cee0e67e
rafikaauda <rafika.auda4@gmail.com>
2023-08-04 15:31:41 +0700

    delete regist from user controll

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -3,45 +3,6 @@ const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 
 class UserController {
-    static async CreateUser(req, res){
-        const {username, email, password} = req.body;
-        try {
-            const hashedPassword = await bcrypt.hash(password, 12);
-            const user = await prisma.user.create({
-                data: {
-                    username,
-                    email,
-                    password:hashedPassword
-                }
-            });
-            
-            // validation empty data
-            if(!username){
-                return res.status(400).json({
-                    result:"failed",
-                    message:"username cannot be empty"
-                });
-            }
-            if(!email){
-                return res.status(400).json({
-                    result:"failed",
-                    message:"email cannot be empty"
-                });
-            }
-            if(!password){
-                return res.status(400).json({
-                    result:"failed",
-                    message:"password cannot be empty"
-                });
-            }
-            res.status(200).json({
-                message:"succes create data"
-            });
-        } catch (error) {
-            res.status(500).json({ msg:"user already exists"})
-        }
-    }
-
     static async getUser(req, res){
         try {
             const users = await prisma.user.findMany({
