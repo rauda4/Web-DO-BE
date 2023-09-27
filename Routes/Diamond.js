39dadb28f779bd21603aaeadf397c709643e5a72
rafikaauda <rafika.auda4@gmail.com>
2023-09-27 17:58:21 +0700

    add new route

--- Routes/Diamond.js
+++ Routes/Diamond.js
@@ -1,4 +1,5 @@
 const diamondController = require('../controllers/Diamond.controller');
+const paymentController = require('../controllers/Payment.controller');
 const Router = require('express').Router();
 
 Router.post('/', diamondController.createDataDiamond);
@@ -6,5 +7,6 @@ Router.get('/', diamondController.getDiamond);
 Router.get('/:id', diamondController.getDiamondById);
 Router.put('/:id', diamondController.updateData);
 Router.delete('/:id', diamondController.deleteDiamond);
+Router.post('/process-transaction', paymentController.createPaymentDiamond);
 
 module.exports = Router;
--- Routes/User.js
+++ Routes/User.js
@@ -1,11 +1,10 @@
-const UserController = require("../controllers/User.controller");
-const authOnly = require("../middlewares/auth");
-const Router = require("express").Router();
+const UserController = require('../controllers/User.controller');
+const authOnly = require('../middlewares/auth');
+const Router = require('express').Router();
 
-Router.get("/", UserController.getUser)
-Router.get("/:id", UserController.GetUserById)
-Router.get("/search/:key", authOnly, UserController.GetUserByQuery)
-Router.put("/:id", authOnly, UserController.updateUser)
-Router.delete("/:id", UserController.deleteUser)
+Router.get('/', UserController.getUser);
+Router.get('/:id', UserController.GetUserById);
+Router.put('/:id', authOnly, UserController.updateUser);
+Router.delete('/:id', UserController.deleteUser);
 
-module.exports = Router
+module.exports = Router;
