096cb8a8b5139be97d4615bdb274868a133460d4
rafikaauda <rafika.auda4@gmail.com>
2023-09-28 23:06:26 +0700

    update routes

--- Routes/index.js
+++ Routes/index.js
@@ -1,14 +1,16 @@
-const router = require("express").Router();
-const UserRouter = require("./User");
-const DiamondRouter = require("./Diamond");
-const ProductRouter = require("./Products")
-const AuthRouter = require("./Auth")
+const router = require('express').Router();
+const UserRouter = require('./User');
+const DiamondRouter = require('./Diamond');
+const ProductRouter = require('./Products');
+const AuthRouter = require('./Auth');
+const PaymentRouter = require('./Payment');
 
-router.use("/users", UserRouter)
-router.use("/diamond", DiamondRouter)
-router.use("/product", ProductRouter)
+router.use('/users', UserRouter);
+router.use('/diamond', DiamondRouter);
+router.use('/product', ProductRouter);
+router.use('/payment', PaymentRouter);
 
 // Auth handler
-router.use("/auth", AuthRouter)
+router.use('/auth', AuthRouter);
 
-module.exports = router
+module.exports = router;
