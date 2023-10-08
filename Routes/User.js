99f8dd6f53f18dd51d29f4a46c2f5857aea40172
rafikaauda <rafika.auda4@gmail.com>
2023-10-08 20:24:40 +0700

    update route userr

--- Routes/User.js
+++ Routes/User.js
@@ -1,10 +1,8 @@
 const UserController = require('../controllers/User.controller');
-const authOnly = require('../middlewares/auth');
 const Router = require('express').Router();
 
-Router.get('/', UserController.getUser);
-Router.get('/:id', UserController.GetUserById);
-Router.put('/:id', authOnly, UserController.updateUser);
-Router.delete('/:id', UserController.deleteUser);
+Router.get('/', UserController.getUsers);
+Router.get('/profile', UserController.selectUser);
+Router.put('/update', UserController.updateUsers);
 
 module.exports = Router;
